f3ec6aac2857f67cbb04583e4d38cfe9be903565
Yuhsak Inoue <yuhsak.inoue@gmail.com>
2023-02-22 20:04:19 +0900

    Fix wrong request type

--- src/fetch/method.ts
+++ src/fetch/method.ts
@@ -1,6 +1,6 @@
 import { reqWithBody, reqWithQuery } from './req'
 
 export const get = reqWithQuery('GET')
-export const del = reqWithBody('DELETE')
+export const del = reqWithQuery('DELETE')
 export const post = reqWithBody('POST')
 export const patch = reqWithBody('PATCH')
