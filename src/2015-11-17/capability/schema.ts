1b1aaac3805908cf75f987d1c25b1a7b7bf776ab
Yuhsak Inoue <yuhsak.inoue@gmail.com>
2024-08-08 20:34:54 +0900

    Update capability schema

--- src/2015-11-17/capability/schema.ts
+++ src/2015-11-17/capability/schema.ts
@@ -6,12 +6,13 @@ export const OpnPaymentsCapabilitySchema = z.object({
   banks: z.string().array(),
   limits: z.record(
     z.object({
-      min: z.number().int(),
-      max: z.number().int(),
+      min: z.number().int().optional(),
+      max: z.number().int().optional(),
     }),
   ),
   payment_backends: z.unknown().array(),
   zero_interest_installments: z.boolean(),
+  tokenization_methods: z.string().array(),
 })
 
 export type OpnPaymentsCapability = z.infer<typeof OpnPaymentsCapabilitySchema>
